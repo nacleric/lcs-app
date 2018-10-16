d4bcc71eee5061e29adffee4dd06a40bc05b38a1
Eric Ng <naclericny@gmail.com>
2018-10-16 02:05:12 -0400

    added notes and comments for psuedo code for index.js

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ window.addEventListener('load', () => {
 
 
 /*
+//put match data in the form of an array!!!
 async function fetchMatch() {
-    const res = await fetch(matchurl)
-    const json = await res.json();
+    const res = await fetch(matchurl) 
+    const json = await res.json();  // array will be fetched and json will hold it
 
     const main = document.querySelector('main');
 
+    //iterate thew all the json data
     json.matches.forEach(#match => {
         const el = document.createElement('match-history');
-        el.match = match;
-        main.appendChild(el);
+        el.match = match; 
+        main.appendChild(el); // elements will be passed down to <main>
     })
 }
 */
