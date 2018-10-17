36a54fb516c181134af98b18774ea2f5d3350c17
Eric Ng <naclericny@gmail.com>
2018-10-17 01:43:26 -0400

    added 1 comment and some html

--- lcsmatch.js
+++ lcsmatch.js
@@ -3,11 +3,20 @@ class LcsMatchHistory extends HTMLElement {
         super()
         //shadow root used to encapsulate styling
         const shadow = this.attachShadow({ mode: 'open' })
+        shadow.innerHTML = `
+            <style>
+                //add css here
+            </style>
+            <h3>Centered text</h3> 
+            <div>
+            </div>
+            <p> hello world </p>
+        `;
     }
     /*
     func setMatch(match) {
     }
     */
 }
-
+// class LcsMatchHistory is passed into 'lcs-match'. turns into custom element 
 customElements.define('lcs-match', LcsMatchHistory)
