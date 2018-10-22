746400555c38a1619bfa4397d5b8cb969669230f
Eric Ng <naclericny@gmail.com>
2018-10-21 20:05:51 -0400

    initialized elements use to pass json

--- button.js
+++ button.js
@@ -1,22 +1,9 @@
-class FactButton extends HTMLElement {
+class FactWindow extends HTMLElement {
     constructor() {
         super()
         //shadow root used to encapsulate styling
         const shadow = this.attachShadow({ mode: 'open' })
         shadow.innerHTML = `
-            <style>
-                //add css here
-            </style>
-            <h3>Centered text</h3> 
-            <div>
-            </div>
-            <p> hello world </p>
         `;
-    }
-    /*
-    func setMatch(match) {
-    }
-    */
-}
-// class CurrentWeather is passed into 'current-weather'. turns into custom element 
-customElements.define('fact-button', FactButton)
+// class FactWindow is passed into 'fact-button'. turns into custom element <fact-window>
+customElements.define('fact-window', FactWindow)
--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-document.getElementById('fetchdata').addEventListener('click', fetchBerry);
+document.getElementById('fetchbutton').addEventListener('click', fetchPokemon);
 	
-//const myRequest = 'https://pokeapi.co/api/v2/berry/oran/'
-function fetchBerry(){
-    console.log("hello world")         
+function fetchPokemon(){
+    const myRequest = 'https://pokeapi.co/api/v2/pokemon/1/'
+    fetch(myRequest)
+        .then(function(response) {
+            return response.json()     
+        })
+        .then(function(myJson) {
+            console.log(JSON.stringify(myJson));
+        });
+        initiElement()
+            
+
+};
+
+function initElement(){
+    const main = document.querySelector('')
+    const pokeType = document.createElement('p')
+    const pokeWeight = document.createElement('p')
+    main.appendChild(pokeType)
+    main.appendChild(pokeWeight)
 }
 
